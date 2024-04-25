289be8286e378f44ecbd291c6fcc39b0df6272f1
GhostFir31 <j3sus0marle@gmail.com>
2024-04-24 20:01:51 -0700

    finish

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
+const cors = require('cors');
 const express = require('express');
 const app = express();
+const fs= require("fs");
+const https = require('https');
 
-const router = require('./router/rutas'); 
+const router = require('./router/rutas');
+
+const llavePrivada = fs.readFileSync('private.key');
+const certificado = fs.readFileSync('certificate.crt');
+
+const credenciales = {key: llavePrivada, cert: certificado,passphrase: 'password'};
+
+const httpsServer = https.createServer(credenciales, app);
 
 const PORT = 3000;
 
-app.use('/', router); 
+app.use(cors());
 
-app.listen(PORT, () => {
+app.use('/', router);
+
+httpsServer.listen(PORT, () => {
+
   console.log("Server running on port "+ PORT);
+
 });
+
